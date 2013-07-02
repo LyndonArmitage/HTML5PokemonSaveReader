256d3ecfca59e6c8d107e72501a985b256052c8f
Lyndon <lyndon.armitage@gmail.com>
2013-07-02 18:11:17 +0100

    Basic code to check that the File API is suppported and fixed CSS to work (forgot rel attribute)

---
+++ js/save_viewer.js
@@ -0,0 +1,30 @@
+/*
+
+JavaScript Based Pokemon Save File viewer by Lyndon Armitage 2013
+---
+This will make use of HTML5's File API so will not work in older browsers.
+
+*/
+
+function loadEvent() {
+	var container = document.getElementById("container");
+	if(!supportsFileAPI()) {
+		container.innerHTML = "Your browser does not support HTML5's File API. Please update to a better browser.";
+	}
+	else {
+		// We support HTML5 File API so let's get cooking.
+	}
+}
+
+/**
+ * Function that returns true if the user's web browser supports the HTML5 File API
+ * @returns {boolean}
+ */
+function supportsFileAPI() {
+	if(window.File && window.FileReader && window.FileList && window.Blob) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
