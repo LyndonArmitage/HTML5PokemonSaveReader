670b2ee66741663c9bd2ead325431b095f37770a
Lyndon <lyndon.armitage@gmail.com>
2013-07-04 00:14:59 +0100

    Made neat output

--- js/main.js
+++ js/main.js
@@ -6,15 +6,13 @@ $(document).ready(function() {
 function loadEvent() {
 	var $container = $("#container");
 	if(!supportsFileAPI()) {
-		$container.text("Your browser does not support HTML5's File API. Please update to a better browser.");
+		$container.html("<div id='error'>Your browser does not support HTML5's File API. Please update to a better browser.</div>");
 	}
 	else {
 		// We support HTML5 File API so let's get cooking.
-		$container.html("<div id='inputLabel'>Select Save File</div><input type='file' id='fileInput' name='savefile' style='display: none'/>");
-		var $fileIn = $container.find("#fileInput");
+		var $fileIn = $("#fileInput");
 		$fileIn.bind("change", loadFile);
-		var $label = $container.find("#inputLabel");
-		$label.bind("click", function() {
+		$("#inputLabel").bind("click", function() {
 			$fileIn.click();
 		})
 	}
@@ -37,14 +35,45 @@ String.prototype.endsWith = function(suffix) {
 	return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
+function error(msg) {
+	$("#outputSection").html("<div id='error'>"+msg+"</div>");
+}
+
 function readFile(savefile) {
 	var reader = new FileReader();
 
 	reader.onload = (function(theFile) {
 		return function(e) {
-			$("#container").append("<div id='saveName'>"+ theFile.name +" loaded</div>");
+			$("#outputSection").html("<h2>"+ theFile.name +" loaded</h2>");
 			var results = parseSav(e.target.result);
-			console.log(results);
+			if(results.checksum === 0) {
+				error("Invalid save file provided. Is it a Generation I save file?");
+			}
+			else {
+				console.log(results);
+				var resultsContents = "<ul id='results'>"+
+						"<li><b>Trainer Name:</b> " + results.trainerName + "</li>"+
+						"<li><b>Trainer ID:</b> " + results.trainerID + "</li>"+
+						"<li><b>Rival Name:</b> " + results.rivalName + "</li>"+
+						"<li><b>Time Played:</b> " + results.timePlayed.hours +":"+ results.timePlayed.minutes + ":" + results.timePlayed.seconds +"</li>";
+				var items = results.pocketItemList;
+				resultsContents += "<li><b>Pocket Items: </b>";
+				if(items.count > 0) {
+					var html = "<ul>";
+					for(var i = 0; i < items.count; i ++) {
+						var item = items.items[i];
+						html += "<li><b>"+item.name+"</b> x"+item.count+"</li>";
+					}
+					html +="</ul>";
+
+					resultsContents += html;
+
+				}
+				resultsContents += "</li>";
+				resultsContents += "</ul>";
+				$("#outputSection").append(resultsContents);
+
+			}
 		};
 	})(savefile);
 	reader.readAsBinaryString(savefile);
@@ -52,15 +81,12 @@ function readFile(savefile) {
 
 function loadFile(evt) {
 	evt = evt.originalEvent || evt;
-	alert(evt);
-	console.log(evt);
 	var savefile = evt.target.files[0];
 	// save files are 32kb large and end in .sav
 	if(savefile != null && savefile.size >= 32768 && savefile.name.endsWith(".sav")) {
-		alert("Yay a valid save file!");
 		readFile(savefile);
 	}
 	else {
-		alert("That wasn't a valid save file");
+		error("Invalid save file provided. File size or extension is wrong.");
 	}
 }
