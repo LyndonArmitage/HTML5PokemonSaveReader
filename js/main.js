770290b133cf30c35efaeca73712e4615795b4c3
Daniell Mesquita <daniellplasma@gmail.com>
2015-09-22 18:03:40 -0300

    Added tip that soon user will can get badges info

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ function readFile(savefile) {
 						"<li><b>Time Played:</b> " + results.timePlayed.hours +":"+ results.timePlayed.minutes + ":" + results.timePlayed.seconds + "</li>"+
 						"<li><b>Money:</b> " + results.money + "</li>"+
 						"<li><b>Checksum:</b> " + results.checksum + "</li>"+
-						"<li><b>Current PC Box:</b> " + results.currentPCBox + "</li></tab><tab id='tabc2' class='tab-content clearfix' style='display: none;'>";
+						"<li><b>Current PC Box:</b> " + results.currentPCBox + "</li><span>Soon, you'll can get your badges and edit them.</span></tab><tab id='tabc2' class='tab-content clearfix' style='display: none;'>";
 
 				function addItemList(label, items) {
 					resultsContents += "<li><b>"+label+": </b>";
